ecd5c184263c5ca920b027e9b1b89236dd6213b1
Loic Galland <lg222sv@student.lnu.se>
2019-09-30 17:54:48 +0200

    Member

--- Workshop2_UML/src/Model/Member.java
+++ Workshop2_UML/src/Model/Member.java
@@ -5,9 +5,23 @@ public class Member {
     private String Personal_Number;
     private int ID;
 
+    public Member(String name, String personal_Number) {
+        Name = name;
+        Personal_Number = personal_Number;
+    }
+
+
     public String getMemberName(){return Name;}
 
     public String getPersonal_Number(){return Personal_Number;}
 
     public int getMemberID(){return ID;}
+
+    public void setMemberName(String name){
+        this.Name = name;
+    }
+
+    public void setPersonal_Number(String personalNumber){
+        this.Personal_Number = personalNumber;
+    }
 }
