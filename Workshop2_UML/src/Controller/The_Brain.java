4ba4636111ecd810d760961aeab98fea58db2aa8
Anas <AK47@MacBook-Pro-AK.local>
2019-09-30 18:00:20 +0200

    BASIC METHODS

--- Workshop2_UML/src/Controller/The_Brain.java
+++ Workshop2_UML/src/Controller/The_Brain.java
@@ -1,4 +1,24 @@
 package Controller;
 
 public class The_Brain {
+
+    public void deleteMember(){
+
+    }
+
+    public void changeMemberData(){
+
+    }
+
+    public void deleteBoat(){
+
+    }
+
+    public void changeBoatData(){
+
+    }
+
+    public void searchData(){
+        
+    }
 }
