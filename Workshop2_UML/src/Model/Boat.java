09584fec6a75405238da4467e58d42c436008428
Anas <AK47@MacBook-Pro-AK.local>
2019-09-30 17:55:48 +0200

    ADDED SETTER AND GETTER

--- Workshop2_UML/src/Model/Boat.java
+++ Workshop2_UML/src/Model/Boat.java
@@ -1,11 +1,40 @@
 package Model;
 
 public class Boat {
-    private String Type;
-    private String Length;
 
-    public String getBoatLength(){return Length;}
+    private String boatType;
+    private double boatLength;
+    private double boatWidth;
 
-    public String getBoatType(){return Type;}
+    public Boat(String boatType, double boatWidth, double boatLength ) {
 
+        this.boatType = boatType;
+        this.boatLength = boatLength;
+        this.boatWidth = boatWidth;
+    }
+
+    public String getBoatType(){
+        return boatType;
+    }
+
+    public double getBoatWidth() {
+        return boatWidth;
+    }
+
+    public double getBoatLength(){
+        return boatLength;
+    }
+
+
+    public void setBoatType(String boatType) {
+        this.boatType = boatType;
+    }
+
+    public void setBoatLength(double boatLength) {
+        this.boatLength = boatLength;
+    }
+
+    public void setBoatWidth(double boatWidth) {
+        this.boatWidth = boatWidth;
+    }
 }
